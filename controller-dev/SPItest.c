081f46c37810a5b24d372c35fa587a0e17c8cd52
TehGelly <itsmanofpopsicle@gmail.com>
2018-11-07 19:43:25 -0600

    fixed and updated code for spi testing

--- controller-dev/SPItest.c
+++ controller-dev/SPItest.c
@@ -5,6 +5,9 @@
 #define BASE_0 128
 #define BASE_1 256
 
+#define OPCODE 0x40
+#define READ 0x01
+
 #define SPI_SPEED 10000000 //10 MHZ
 
 #define usingMCPLIB 1
@@ -25,12 +28,22 @@ int main(){
 			pinMode(BASE_1 + i, OUTPUT);
 		}
 	
-		//now we flicker
+		//to test operation, we turn on the half of upper bank of both pins, half off
+		//honestly just to see wtf is actually going on
+		
+		for(int i = 8; i < 16; i+=2){
+			digitalWrite(BASE_0 + i, 0);
+			digitalWrite(BASE_0 + i + 1, 1);
+			digitalWrite(BASE_1 + i, 1);
+			digitalWrite(BASE_1 + i + 1, 0);
+		}
+		
+		//flicker the rest
 		while(1){
 		
-			for(int i = 0; i < 16; i++){
-				digitalWrite(BASE_0 + i, (i&1)^STATE)
-				digitalWrite(BASE_1 + i, (i&1)^STATE^1)
+			for(int i = 0; i < 8; i++){
+				digitalWrite(BASE_0 + i, (i&1)^STATE);
+				digitalWrite(BASE_1 + i, (i&1)^STATE^1);
 			}
 		
 			STATE ^= 1;
@@ -46,5 +59,14 @@ int main(){
 		printf("what is fd1? %d", fd1);
 		
 		//so now we need to figure out, like, what the hell is actually going on in terms of writing
+		//to write to a certain address, we need three bytes in an array
+		char* regCMD = malloc(sizeof(char)* 2);
+		//first, we make the control byte for MCP0
+		*(regCMD) = OPCODE |  ((0x0) << 1); //write operation to 000
+		*(regCMD + 1) = 0x12; //address for GPIOA register, the right side
+		*(regCMD + 2) = 0xAA; //easy to test
+		
+		wiringPiSPIODataRW(0,regCMD,3);
+		//this should work, no other setup necessary?
 	}
 }
