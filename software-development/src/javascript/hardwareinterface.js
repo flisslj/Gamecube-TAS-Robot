17ea69996992d7ed6639bc26c058a36b777e3e8c
Luke Fliss <flisslj@ad.msoe.edu>
2018-12-02 20:13:59 -0600

    added requests for most methods, except for the ones which require post requests

--- software-development/src/javascript/hardwareinterface.js
+++ software-development/src/javascript/hardwareinterface.js
@@ -4,100 +4,131 @@
 
  function HardwareInterface (){
      var http = require('http');
-     let url = "";
-     let secret = "";
+     let url = "http://" + "localhost:";
+     let port = "80"
+     let secret = "&secret=" + "password1234";
+     let request;
      
      let heartBeat = function(){
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
+        })
      }
 
      let getAttachedHardware = function() {
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
+        })
      }
 
      let getSDSize = function() {
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
+        })
      }
 
      let openSpace = function() {
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
+        })
      }
 
      let getFiles = function() {
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
+        })
      }
 
      let getISOs = function() {
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
+        })
      }
 
      let getTASes = function() {
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
+        })
      }
 
      let currentISO = function() {
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
+        })
      }
 
      let currentTAS = function() {
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
+        })
      }
     
      let setTas = function(){
-
+        request = "";
+        //these need to be post requests
      }
 
      let setISO = function(){
-
+        request = "";
+        //these need to be post requests
      }
 
      let loadISO = function(){
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
+        })
      }
 
      let loadTAS = function(){
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
+        })
      }
 
      let run = function(){
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
-    }
+        })
+     }
+
+     let abort = function(){
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
+
+        })
+     }
+     
+     let getMemoryDump = function(){
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
+
+        })
+     }
     
-    let abort = function(){
+     let getMemoryDumpData = function(){
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
-    }
-    let getMemoryDump = function(){
+        })
+     }
+    
+     let clearMemoryDump = function(){
+        request = "";
+        http.get(url+port+request+parameters+secret, (resp)=>{
 
-    }
-    let getMemoryDumpData = function(){
-
-    }
-    let clearMemoryDump = function(){
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        })
+     }
 
  }
