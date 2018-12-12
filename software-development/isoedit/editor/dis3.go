08bfc4c920f4d0762bc82cc124f122021814576b
nickgregerson <gregersonnd@msoe.edu>
2018-12-12 11:15:41 -0600

    Added files for some of the disassembly patterns Also dis4 is going to be hell in comparison to everything else

--- software-development/isoedit/editor/dis3.go
+++ software-development/isoedit/editor/dis3.go
@@ -1,6 +1,7 @@
 package editor
 
 func dis3Start(num uint32, comm command) (uint32, command) {
-
+	//Grab bits from 6-20, 11-15 adn 16-31 and place them in the command
+	//Add twl to the command name
 	return 0, command{}
 }
