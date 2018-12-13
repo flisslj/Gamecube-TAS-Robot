813490287437e30eca46e10eb6022e8cdb37deb4
nickgregerson <gregersonnd@msoe.edu>
2018-12-13 07:32:15 -0600

    Added a lot of disassembly code

--- software-development/isoedit/editor/dis16.go
+++ software-development/isoedit/editor/dis16.go
@@ -1,6 +1,34 @@
 package editor
 
-func dis16Start(num uint32, comm command) (uint32, command) {
+import "strconv"
 
-	return 0, command{}
+func dis16Start(num uint32, comm command) (uint32, command) {
+	if num == 0 {
+
+		output := uint32((comm.cmdSimple[0]) + (comm.cmdSimple[1] << 6) + (comm.cmdSimple[2] << 11) + (comm.cmdSimple[3] << 16))
+
+		return output, comm
+	}
+	NAME := ""
+	BO := getBits(6, 10, num)
+	BI := getBits(11, 15, num)
+	BD := getBits(16, 29, num)
+	TYPE := getBits(30, 31, num)
+	if TYPE == 0 {
+		NAME = "BC"
+	} else if TYPE == 1 {
+		NAME = "BCL"
+	} else if TYPE == 2 {
+		NAME = "BCA"
+	} else {
+		NAME = "BCLA"
+	}
+	c := command{
+		cmdName:   NAME,
+		cmdString: NAME + "\t" + strconv.Itoa(BO) + ", " + strconv.Itoa(BI) + ", " + strconv.Itoa(BD),
+		cmdSimple: []int{16, BO, BI, BD, TYPE},
+		asBytes:   num,
+	}
+
+	return num, c
 }
--- software-development/isoedit/editor/dis29.go
+++ software-development/isoedit/editor/dis29.go
@@ -2,7 +2,7 @@ package editor
 
 import "strconv"
 
-func dis24Start(num uint32, comm command) (uint32, command) {
+func dis29Start(num uint32, comm command) (uint32, command) {
 	if num == 0 {
 
 		output := uint32((comm.cmdSimple[0]) + (comm.cmdSimple[1] << 6) + (comm.cmdSimple[2] << 11) + (comm.cmdSimple[3] << 16))
@@ -15,8 +15,8 @@ func dis24Start(num uint32, comm command) (uint32, command) {
 	UIMM := getBits(16, 31, num)
 
 	c := command{
-		cmdName:   "ORI",
-		cmdString: "ORI\tR" + strconv.Itoa(S) + ", R" + strconv.Itoa(A) + ", " + strconv.Itoa(UIMM),
+		cmdName:   "ANDIS.",
+		cmdString: "ANDIS.\tR" + strconv.Itoa(S) + ", R" + strconv.Itoa(A) + ", " + strconv.Itoa(UIMM),
 		cmdSimple: []int{23, S, A, UIMM},
 		asBytes:   num,
 	}
