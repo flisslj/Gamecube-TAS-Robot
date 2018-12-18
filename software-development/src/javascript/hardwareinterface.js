9cdf5cd8439ecf32889c426a00c0b7973cb6e970
Luke Fliss <flisslj@ad.msoe.edu>
2018-12-17 21:12:23 -0600

    reimplemented requests - post requests

--- software-development/src/javascript/hardwareinterface.js
+++ software-development/src/javascript/hardwareinterface.js
@@ -5,7 +5,7 @@ var fs = require('fs'),
  */
 
  function HardwareInterface(){
-     let url = "localhost";
+     let url = "http://localhost:";
      let port = "80"
      let secret = "?secret=" + "password1234";
      let api = "";
@@ -13,9 +13,141 @@ var fs = require('fs'),
      this.heartBeat = function(){
          api= "/api/Heartbeat";
          var heartbeat;
-         request('http://localhost/api/Heartbeat?secret=password1234', function(err, res, body){
+         request(url+port+api+secret, function(err, res, body){
             heartbeat = body;
          });
          return heartbeat;
      }
+
+     this.getAttachedHardware = function(){
+        api="api/GetAttachedHardware";
+        var response;
+        request(url+port+api+secret , function(err, res, body){
+            response = body;
+        });
+        return response;
+     }
+     this.getSDSize = function(){
+        api="api/GetSDSize";
+        var response;
+        request(url+port+api+secret , function(err, res, body){
+            response = body;
+        });
+        return response;
+     }
+     this.openSpace = function(){
+        api="api/OpenSpace";
+        var response;
+        request(url+port+api+secret , function(err, res, body){
+            response = body;
+        });
+        return response;
+     }
+     this.getFiles = function(){
+        api="api/GetFiles";
+        var response;
+        request(url+port+api+secret , function(err, res, body){
+            response = body;
+        });
+        return response;
+     }
+     this.getISOs = function(){
+        api="api/GetISOs";
+        var response;
+        request(url+port+api+secret , function(err, res, body){
+            response = body;
+        });
+        return response;
+     }
+     this.getTases = function(){
+        api="api/GetTases";
+        var response;
+        request(url+port+api+secret , function(err, res, body){
+            response = body;
+        });
+        return response;
+     }
+     this.currentISO = function(){
+        api="api/CurrentISO";
+        var response;
+        request(url+port+api+secret , function(err, res, body){
+            response = body;
+        });
+        return response;
+     }
+   this.currentTAS = function(){
+      api="api/CurrentTAS";
+      var response;
+      request(url+port+api+secret , function(err, res, body){
+            response = body;
+      });
+      return response;
+   }    
+   /** post requests needs work
+     this.setTAS = function(tasPath){
+        api="api/SetTAS";
+        request(url+port+api+secret , function(err, res, body){
+
+        })
+     }
+     this.setISO = function(tasPath){
+        api="api/SetISO";
+        request(url+port+api+secret , function(err, res, body){
+
+        })
+     }
+     this.loadISO = function(filePath, data){
+        api="api/";
+        request(url+port+api+secret , function(err, res, body){
+
+        })
+     }
+     this.loadTAS = function(filePath, data){
+        api="api/";
+        request(url+port+api+secret , function(err, res, body){
+
+        })
+     }
+
+     **/
+     this.run = function(){
+        api="api/Run";
+        var response;
+        request(url+port+api+secret , function(err, res, body){
+            response = body;
+        });
+        return response;
+     }
+     this.abort = function(){
+        api="api/Abort";
+        var response;
+        request(url+port+api+secret , function(err, res, body){
+            response = body;
+        });
+        return response;
+     }
+     this.getMemoryDump = function(){
+        api="api/GetMemoryDump";
+        var response;
+        request(url+port+api+secret , function(err, res, body){
+            response = body;
+        });
+        return response;
+     }
+     this.getMemoryDumpData = function(){
+        api="api/GetMemoryDumpData";
+        var response;
+        request(url+port+api+secret , function(err, res, body){
+            response = body;
+        });
+        return response;
+     }
+   this.clearMemoryDump = function(){
+      api="api/clearMemorydump";
+      var response;
+      request(url+port+api+secret , function(err, res, body){
+            response = body;
+      });
+      return response;
+   }     
  }
