edc42bc6016ebdbe05b09cbf8184232aad9f7481
Luke Fliss <flisslj@ad.msoe.edu>
2018-12-19 17:13:26 -0600

    added comments to code

--- software-development/src/javascript/hardwareinterface.js
+++ software-development/src/javascript/hardwareinterface.js
@@ -2,6 +2,7 @@ var fs = require('fs'),
    request = require('request');
 /**
  * this class is used for communicating between the application and the hardware
+ * over ethernet / wifi
  */
 
  function HardwareInterface(){
@@ -10,6 +11,10 @@ var fs = require('fs'),
      let secret = "?secret=" + "password1234";
      let api = "";
      
+     /**
+      * checks the connection between the controller and application
+      * @param callback function for handeling returned data
+      */
      this.heartBeat = function(callback){
          api= "/api/Heartbeat";
          var heartbeat;
@@ -20,18 +25,23 @@ var fs = require('fs'),
 
          });
      }
-
+     /**
+      * checks what hardware components are attached to the controller
+      * gives version numbers and size of sd card if attached
+      */
      this.getAttachedHardware = function(callback){
-        api="api/GetAttachedHardware";
+        api="/api/GetAttachedHardware";
         var response;
         request(url+port+api+secret , function(err, res, body){
             response = body;
             callback(response)
         });
-        
      }
+     /**
+      * check the current size of the sd card
+      */
      this.getSDSize = function(callback){
-        api="api/GetSDSize";
+        api="/api/GetSDSize";
         var response;
         request(url+port+api+secret , function(err, res, body){
             response = body;
@@ -39,8 +49,11 @@ var fs = require('fs'),
         });
         
      }
+     /**
+      * checks how much space is open on the sd card
+      */
      this.openSpace = function(callback){
-        api="api/OpenSpace";
+        api="/api/OpenSpace";
         var response;
         request(url+port+api+secret , function(err, res, body){
             response = body;
@@ -48,8 +61,13 @@ var fs = require('fs'),
         });
         
      }
+
+     /**
+      * get which files are currently on the sd card
+      * including both replay files and isos
+      */
      this.getFiles = function(callback){
-        api="api/GetFiles";
+        api="/api/GetFiles";
         var response;
         request(url+port+api+secret , function(err, res, body){
             response = body;
@@ -57,8 +75,12 @@ var fs = require('fs'),
         });
         
      }
+
+     /**
+      * find out what isos are currently loaded onto the sd card
+      */
      this.getISOs = function(callback){
-        api="api/GetISOs";
+        api="/api/GetISOs";
         var response;
         request(url+port+api+secret , function(err, res, body){
             response = body;
@@ -66,8 +88,12 @@ var fs = require('fs'),
         });
         
      }
+
+     /**
+      * find out which replay files are loaded onto the sd card
+      */
      this.getTases = function(callback){
-        api="api/GetTases";
+        api="/api/GetTases";
         var response;
         request(url+port+api+secret , function(err, res, body){
             response = body;
@@ -75,8 +101,13 @@ var fs = require('fs'),
         });
         
      }
+
+     /**
+      * find out which iso will be launched on the gamecube
+      * when a replay is run
+      */
      this.currentISO = function(callback){
-        api="api/CurrentISO";
+        api="/api/CurrentISO";
         var response;
         request(url+port+api+secret , function(err, res, body){
             response = body;
@@ -84,8 +115,12 @@ var fs = require('fs'),
         });
         
      }
+     /**
+      * find out which replay file will be read when a replay is
+      * launched
+      */
    this.currentTAS = function(callback){
-      api="api/CurrentTAS";
+      api="/api/CurrentTAS";
       var response;
       request(url+port+api+secret , function(err, res, body){
             response = body;
@@ -95,33 +130,38 @@ var fs = require('fs'),
    }    
    /** post requests needs work
      this.setTAS = function(tasPath){
-        api="api/SetTAS";
+        api="/api/SetTAS";
         request(url+port+api+secret , function(err, res, body){
 
         })
      }
      this.setISO = function(tasPath){
-        api="api/SetISO";
+        api="/api/SetISO";
         request(url+port+api+secret , function(err, res, body){
 
         })
      }
      this.loadISO = function(filePath, data){
-        api="api/";
+        api="/api/";
         request(url+port+api+secret , function(err, res, body){
 
         })
      }
      this.loadTAS = function(filePath, data){
-        api="api/";
+        api="/api/";
         request(url+port+api+secret , function(err, res, body){
 
         })
      }
 
      **/
+
+     /**
+      * run a replay using the currently loaded replay file and 
+      * iso
+      */
      this.run = function(callback){
-        api="api/Run";
+        api="/api/Run";
         var response;
         request(url+port+api+secret , function(err, res, body){
             response = body;
@@ -129,8 +169,12 @@ var fs = require('fs'),
         });
         
      }
+
+     /**
+      * interrupt a currently running replay
+      */
      this.abort = function(callback){
-        api="api/Abort";
+        api="/api/Abort";
         var response;
         request(url+port+api+secret , function(err, res, body){
             response = body;
@@ -138,8 +182,11 @@ var fs = require('fs'),
         });
         
      }
+     /**
+      * get the next memory dump to be loaded from the gamecube 
+      */
      this.getMemoryDump = function(callback){
-        api="api/GetMemoryDump";
+        api="/api/GetMemoryDump";
         var response;
         request(url+port+api+secret , function(err, res, body){
             response = body;
@@ -147,8 +194,12 @@ var fs = require('fs'),
         });
         
      }
+
+     /**
+      * get technical information on the next memory dump to be loaded
+      */
      this.getMemoryDumpData = function(callback){
-        api="api/GetMemoryDumpData";
+        api="/api/GetMemoryDumpData";
         var response;
         request(url+port+api+secret , function(err, res, body){
             response = body;
@@ -156,8 +207,12 @@ var fs = require('fs'),
         });
         
      }
+
+     /**
+      * clear all memory dumps waited to be recieved
+      */
    this.clearMemoryDump = function(callback){
-      api="api/clearMemorydump";
+      api="/api/clearMemorydump";
       var response;
       request(url+port+api+secret , function(err, res, body){
             response = body;
